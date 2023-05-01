f96b7379d7c840961d2934b44cad48398de06e02
dbeley <6568955+dbeley@users.noreply.github.com>
2023-05-01 17:40:33 +0200

    Add /fr prefix with i18n_patterns

--- ytvd/urls.py
+++ ytvd/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("ratings.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("i18n/", include("django.conf.urls.i18n")),
 ]
+
+urlpatterns += i18n_patterns(
+    path("", include("ratings.urls")), prefix_default_language=False
+)
