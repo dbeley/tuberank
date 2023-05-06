2b88de0053b0a4a136931d85db224e14ae7d3dbd
dbeley <6568955+dbeley@users.noreply.github.com>
2023-05-06 23:31:22 +0200

    Fix default charts, fix tag selection

--- ratings/charts/views.py
+++ ratings/charts/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.db.models import Avg, Count, Max
+from django.db.models import Avg, Count, Max, F
 from django.http import HttpResponseRedirect
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
@@ -18,9 +18,9 @@ class ChartsView(APIView):
         tags = _get_validated_tags().order_by("name")
         videos = Video.objects.annotate(
             num_ratings=Count("ratings"),
-            avg_rating=Avg("ratings__rating"),
-            count_views=Max("snapshots__count_views"),
-        ).order_by("-avg_rating")
+            avg_rating=Avg(F("ratings__rating")),
+            count_views=Max(F("snapshots__count_views")),
+        )
         if sort_method := request.GET.get("sort_by"):
             if sort_method not in [
                 "newest",
@@ -42,9 +42,11 @@ class ChartsView(APIView):
             elif sort_method == "ratings_count":
                 videos = videos.order_by("-num_ratings")
             elif sort_method == "rating":
-                # default sorting mechanism
-                # videos = videos.order_by("-avg_rating")
-                pass
+                videos = videos.order_by("-avg_rating")
+        else:
+            # default
+            videos = videos.order_by("-avg_rating")
+
         if selected_tag := request.GET.get("tag"):
             tag = UserTag.objects.get(name=selected_tag)
             videos = videos.filter(tags__in=[tag])
@@ -53,7 +55,6 @@ class ChartsView(APIView):
         page = paginator.get_page(request.GET.get("page", 1))
         return Response(
             {
-                "videos": page,
                 "page": page,
                 "tags": tags,
                 "selected_sort_method": sort_method,
