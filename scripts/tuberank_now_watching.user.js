23bab1affa9de31b52226a9dd950efafa3a57d4f
dbeley <6568955+dbeley@users.noreply.github.com>
2024-08-20 14:41:22 +0200

    userscript: trigger on all of youtube with timer

--- scripts/tuberank_now_watching.user.js
+++ scripts/tuberank_now_watching.user.js
@@ -1,7 +1,7 @@
 ﻿  // ==UserScript==
   // @name        TubeRank - Now Watching
   // @namespace   Violentmonkey Scripts
-  // @include     https://www.youtube.com/watch?v=*
+  // @include     https://www.youtube.com/*
   // @grant       GM_xmlhttpRequest
   // @version     1.1
   // @author      dbeley
@@ -11,41 +11,49 @@
   let username = 'changeme';
   let password = 'changeme';
   let url = 'https://tuberank.org/en/api/now-watching/';
-  var fireOnHashChangesToo    = true;
-  var timer;
-  var pageURLCheckTimer       = setInterval (
-    function () {
-        if (   this.lastPathStr  !== location.pathname
-            || this.lastQueryStr !== location.search
-            || (fireOnHashChangesToo && this.lastHashStr !== location.hash)
-        ) {
-          clearTimeout(timer);
-          this.lastPathStr  = location.pathname;
-          this.lastQueryStr = location.search;
-          this.lastHashStr  = location.hash;
-          durationSpan = document.getElementsByClassName("ytp-time-duration")[0].innerHTML;
-          var seconds = +(durationSpan.split(':').reduce((acc,time) => (60 * acc) + +time));
-          var secondsToWait = parseInt(seconds/2);
-          console.log(`New video detected, waiting ${secondsToWait} seconds to send to TubeRank`);
-          timer = setTimeout(gmMain, secondsToWait * 1000);
-        }
-    }
-    , 5000
-  );
+  let fireOnHashChangesToo = true;
+  let timer;
 
-  function gmMain () {
+  function checkAndRunScript() {
+    let currentURL = window.location.href;
+    if (currentURL.includes('watch?v=')) {
+      let durationSpan = document.getElementsByClassName("ytp-time-duration")[0];
+      if (durationSpan) {
+        let seconds = +(durationSpan.innerHTML.split(':').reduce((acc, time) => (60 * acc) + +time));
+        let secondsToWait = parseInt(seconds / 4);
+        console.log(`New video detected, waiting ${secondsToWait} seconds to send to TubeRank`);
+        clearTimeout(timer);
+        timer = setTimeout(gmMain, secondsToWait * 1000);
+      }
+    }
+  }
+
+  function gmMain() {
     let authentication = btoa(username + ":" + password);
     let url1 = window.location.href;
-    let youtubeId = url1.split("watch?v=")[1].split("&list=")[0];
+    let youtubeId = url1.split("watch?v=")[1].split("&")[0];
     console.log(`TubeRank: sending "now watching" request to ${url}${youtubeId}`);
     GM_xmlhttpRequest({
-        method: 'POST',
-        url: `${url}${youtubeId}`,
-        headers: {
+      method: 'POST',
+      url: `${url}${youtubeId}`,
+      headers: {
         'Accept': 'application/json',
         "Content-Type": "application/json",
         "Authorization": `Basic ${authentication}`
-        }
+      }
     });
-  };
-  })();
+  }
+
+  setInterval(function () {
+    if (   this.lastPathStr !== location.pathname
+        || this.lastQueryStr !== location.search
+        || (fireOnHashChangesToo && this.lastHashStr !== location.hash)
+    ) {
+      this.lastPathStr = location.pathname;
+      this.lastQueryStr = location.search;
+      this.lastHashStr = location.hash;
+      checkAndRunScript();
+    }
+  }, 5000);
+
+})();
